a0270fed5efe14c6cf5eb1eeca603b92a2564e89
sINFmeasla <mehmet.aslan@stud.th-rosenheim.de>
2021-12-13 11:11:03 +0100

    Init project

---
+++ projects/chat-server/src/main/kotlin/com/bongofriend/plugins/HTTP.kt
@@ -0,0 +1,23 @@
+package com.bongofriend.plugins
+
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun Application.configureHTTP() {
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+    }
+
+}
---
+++ projects/chat-server/src/test/kotlin/com/bongofriend/ApplicationTest.kt
@@ -0,0 +1,35 @@
+package com.bongofriend
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.websocket.*
+import java.time.*
+import io.ktor.jackson.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.auth.*
+import io.ktor.util.*
+import io.ktor.auth.jwt.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import com.bongofriend.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ configureRouting() }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello World!", response.content)
+            }
+        }
+    }
+}
