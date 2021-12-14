97448c414c4250287b8292a04edb1be16dfc75fc
sINFmeasla <mehmet.aslan@stud.th-rosenheim.de>
2021-12-14 12:32:02 +0100

    Clean up test methods

--- projects/chat-server/src/test/kotlin/com/bongofriend/TestUtils.kt
+++ projects/chat-server/src/test/kotlin/com/bongofriend/TestUtils.kt
@@ -1,14 +1,30 @@
 package com.bongofriend
 
+import com.typesafe.config.ConfigFactory
 import io.ktor.config.*
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
+import io.ktor.server.testing.*
 import kotlin.io.path.Path
 import kotlin.io.path.deleteIfExists
 
-fun prepareDatabase(config: ApplicationConfig) {
-    val dbConfig = config.config("db")
+object TestUser {
+    const val username = "hans_peter"
+    const val password = "password"
+}
+
+private const val configName = "application_test.conf"
+
+fun prepareDatabase() {
+    val appConfig = HoconApplicationConfig(ConfigFactory.load(configName))
+    val dbConfig = appConfig.config("db")
     val dbPath = dbConfig.property("url").getString().replace("jdbc:sqlite:", "")
     Path(dbPath).deleteIfExists()
+}
+
+fun withChatServerEnvironment(func: TestApplicationEngine.() -> Unit) {
+    val env = createTestEnvironment {
+        config = HoconApplicationConfig(ConfigFactory.load(configName))
+    }
+    withApplication(env) {
+        this.func()
+    }
 }
--- projects/chat-server/src/test/kotlin/com/bongofriend/UserTest.kt
+++ projects/chat-server/src/test/kotlin/com/bongofriend/UserTest.kt
@@ -1,45 +1,27 @@
 package com.bongofriend
 
-import com.bongofriend.plugins.configureKoin
-import com.bongofriend.plugins.configureRouting
-import com.bongofriend.plugins.configureSerialization
 import com.bongofriend.requests.AddNewUserRequest
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.typesafe.config.ConfigFactory
-import io.ktor.config.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import io.netty.handler.codec.http.HttpHeaders.addHeader
-import org.jetbrains.exposed.sql.SchemaUtils
-import java.io.File
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.io.path.*
 
 class UserTest {
-    private val testUser = object {
-        val username = "hans_peter"
-        val password = "password"
-    }
-
-    private val testConfigName = "application_test.conf"
     private val objectMapper = ObjectMapper()
-    private val testEnv = createTestEnvironment {
-        config = HoconApplicationConfig(ConfigFactory.load(testConfigName))
-    }
 
     @BeforeTest
     fun clearTestDatabase() {
-        prepareDatabase(testEnv.config)
+        prepareDatabase()
     }
 
     @Test
     fun testNewUserCreation() {
-        withApplication(testEnv) {
+        withChatServerEnvironment {
             with(handleRequest(HttpMethod.Post, "/users"){
                 addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-                val request = AddNewUserRequest(testUser.username, testUser.password)
+                val request = AddNewUserRequest(TestUser.username, TestUser.password)
                 setBody(objectMapper.writeValueAsString(request))
             }) {
                 assertEquals(HttpStatusCode.Created, response.status())
